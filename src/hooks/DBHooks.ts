31547df6e7402abb5d7a3db52adebc27f8c7078f
aihki <laine.nestori.julius@gmail.com>
2025-01-20 09:48:42 +0200

    first

--- src/hooks/DBHooks.ts
+++ src/hooks/DBHooks.ts
@@ -1,10 +1,19 @@
 import { useEffect } from "react";
+import Loki from "lokijs";
 
 const useDB = () => {
     useEffect(() => {
         try {
+            const dbInstance = new Loki('face.json');
+            // Load the database i it exists
+            dbInstance.loadDatabase({}, () => {
+                const collection = dbInstance.getCollection('faces');
+                if (!collection) {
+                    dbInstance.addCollection('faces');
+                }
+            });
         } catch (error) {
-            console.error("Error loading models:", error);
+            console.error("useDB error:", error);
         }
     }, []);
 };
